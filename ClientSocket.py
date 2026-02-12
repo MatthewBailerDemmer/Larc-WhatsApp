ae52d63210f0724260378301fc8730ed4dff68a9
Matheus Bailer Demmer <mbdemmer@furb.br>
2026-02-11 21:15:26 -0300

    funcionando ihulll

--- ClientSocket.py
+++ ClientSocket.py
@@ -5,37 +5,39 @@ import time
 
 
 class ClientSocket:
-    def __init__(self, HOST, PORT, userId, password):
+    def __init__(self, HOST, PORT):
         self.HOST = HOST
         self.PORT = PORT
-        self.userId = userId
-        self.password = password
-        self.stop_event = threading.Event()
-        self.threadKeepAlive = threading.Thread(target=self.KeepAlive, args=(self.stop_event,))
+        
 
 
 
     def KeepAlive(self, stop_event):
         while not stop_event.is_set():
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect((self.HOST, self.PORT))
-                msg = f"GET USERS {self.userId}:{self.password}\r\n"
-                s.sendall(msg.encode("ascii"))
-                data = s.recv(1024)
+                #s.connect((self.HOST, self.PORT))
+                #msg = f"GET USERS {self.userId}:{self.password}\r\n"
+                #s.sendall(msg.encode("ascii"))
+                #data = s.recv(1024)
                 #print(f"Logged In: {data.decode("utf-8")!r}")
                 time.sleep(6)
                 
     def get_score(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect((self.HOST, self.PORT))
-                msg = f"GET USERS {self.userId}:{self.password}\r\n"
-                s.sendall(msg.encode("ascii"))
-                data = s.recv(1024)
-                print(f"Logged In: {data.decode("utf-8")!r}")
-                return data.decode("utf-8")
+                #s.connect((self.HOST, self.PORT))
+                #msg = f"GET USERS {self.userId}:{self.password}\r\n"
+                #s.sendall(msg.encode("ascii"))
+                #data = s.recv(1024)
+                #print(f"Logged In: {data.decode("utf-8")!r}")
+                #return data.decode("utf-8")
+                return "222:Kid Bengala:0:1943:Matheus Bailer:2:69:tadalafelas:0"
 
 
-    def log_in(self):
+    def log_in(self, username, password):
+        self.userId = username
+        self.password = password
+        self.stop_event = threading.Event()
+        self.threadKeepAlive = threading.Thread(target=self.KeepAlive, args=(self.stop_event,))
         self.threadKeepAlive.start()
     def log_out(self):
         self.stop_event.set()
@@ -43,46 +45,51 @@ class ClientSocket:
 
     def get_messages(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.HOST, self.PORT))
-            msg = f"GET MESSAGE {self.userId}:{self.password}\r\n"
-            s.sendall(msg.encode("ascii"))
-            data = s.recv(1024)
-            print(f"Messages received: {data.decode("utf-8")!r}")
+            #s.connect((self.HOST, self.PORT))
+            #msg = f"GET MESSAGE {self.userId}:{self.password}\r\n"
+            #s.sendall(msg.encode("ascii"))
+            #data = s.recv(1024)
+            #print(f"Messages received: {data.decode("utf-8")!r}")
+            return "1943:Hallow"
 
     def send_message(self, user2Id, sending):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.settimeout(5)
-            msg = f"SEND MESSAGE {self.userId}:{self.password}:{user2Id}:{sending}\r\n".encode("ascii")
-            s.sendto(msg ,(self.HOST, 1011))
+            #s.settimeout(5)
+            #msg = f"SEND MESSAGE {self.userId}:{self.password}:{user2Id}:{sending}\r\n".encode("ascii")
+            #s.sendto(msg ,(self.HOST, 1011))
             try:
-                data, addr = s.recvfrom(1024)
-                print(f"Sent message: {data.decode("utf-8")!r}")
+                #data, addr = s.recvfrom(1024)
+                #print(f"Sent message: {data.decode("utf-8")!r}")
+                print(f"{user2Id}: {sending}")
             except socket.timeout:
                 print("No message received from server")
 
     def get_players(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.HOST, self.PORT))
-            msg = f"GET PLAYERS {self.userId}:{self.password}\r\n"
-            s.sendall(msg.encode("ascii"))
-            data = s.recv(1024)
-            print(f"Players: {data.decode("utf-8")!r}")
+            #s.connect((self.HOST, self.PORT))
+            #msg = f"GET PLAYERS {self.userId}:{self.password}\r\n"
+            #s.sendall(msg.encode("ascii"))
+            #data = s.recv(1024)
+            #print(f"Players: {data.decode("utf-8")!r}")
+            return "222:PLAYING:1943:IDLE:69:GETTING"
 
     def get_card(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.HOST, self.PORT))
-            msg = f"GET CARD {self.userId}:{self.password}\r\n"
-            s.sendall(msg.encode("ascii"))
-            data = s.recv(1024)
-            print(f"CARD: {data.decode("utf-8")!r}")
+            #s.connect((self.HOST, self.PORT))
+            #msg = f"GET CARD {self.userId}:{self.password}\r\n"
+            #s.sendall(msg.encode("ascii"))
+            #data = s.recv(1024)
+            #print(f"CARD: {data.decode("utf-8")!r}")
+            return "J:DIAMOND"
 
     def send_message_game(self, msg):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.settimeout(5)
-            msg = f"SEND GAME {self.userId}:{self.password}:{msg}\r\n".encode("ascii")
-            s.sendto(msg ,(self.HOST, 1011))
-            try:
-                data, addr = s.recvfrom(1024)
-                print(f"Sent message: {data.decode("utf-8")!r}")
-            except socket.timeout:
-                print("No message received from server")
+            #s.settimeout(5)
+            #msg = f"SEND GAME {self.userId}:{self.password}:{msg}\r\n".encode("ascii")
+            #s.sendto(msg ,(self.HOST, 1011))
+            #try:
+            #    data, addr = s.recvfrom(1024)
+            #    print(f"Sent message: {data.decode("utf-8")!r}")
+            #except socket.timeout:
+            #    print("No message received from server")
+            print(msg)
--- main.py
+++ main.py
@@ -5,48 +5,149 @@ import requests
 
 HOST = "larc.furb.br"
 PORT = 1012
-userId = 1943
-password = "dghol"
+logged_userId = 0
+password = ""
+
+#userId = 1943
+#password = "dghol"
+
+LAST_ACTION = ""
+
+
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+
+client = ClientSocket.ClientSocket(HOST, PORT)
+
+
+def getName(result, userId):
+    name = ''
+    for seila in result:
+        if seila[0] == userId:
+            name = seila[1]
+            break
+    return name
+
+
+def players_front(players, result):
+    players_retz = []
+
+    i = 0
+
+    while i < len(players) - 1:
+        name = ''
+        for seila in result:
+            print(seila[0])
+            print(players[i])
+            if seila[0] == players[i]:
+                name = seila[1]
+                break
+
+        tuplezinha  = (name, players[i + 1])
+        players_retz.append(tuplezinha)
+        i += 2
+
+    return players_retz
+
+
+def render_car(game_action, processed_card):
+    if game_action == "ENTER" or game_action == "STOP" or game_action == "QUIT":
+        client.send_message_game(game_action)
+    elif game_action == "PEGAR":
+        processed_card = process_card(client.get_card())
+    return processed_card
 
 
 
-app = Flask(__name__)
-
-client = ClientSocket.ClientSocket(HOST, PORT, userId, password) 
-
-client.log_in()
-time.sleep(15)
-
-#while True:
-#    option = int(input(f"1 para entrar no jogo\n2 para parar\n3 para sair do jogo\n4 para pegar carta\n5 para ver os players\n6 para ver logados\n"))
-#    if option == 1:
-#        client.send_message_game("ENTER")
-#    elif option == 2:
-#        client.send_message_game("STOP")
-#    elif option == 3:
-#        client.send_message_game("QUIT")
-#    elif option == 4:
-#        client.get_card()
-#    elif option == 5:
-#        client.get_players()
-#    elif option == 6:
-#        client.get_score()
+def process_card(card):
+    return card.replace(":","")
 
 
 @app.route('/')
+def login():
+    user = request.args.get("logged_in_username")
+    password = request.args.get("logged_in_passwrod")
+
+
+    if user is not None and password is not None:
+        logged_userId = user
+        password = password
+        client.log_in(logged_userId, password)
+        time.sleep(6)
+        return get_all_posts()
+
+    
+
+    return render_template("login.html")
+
+
+
+
+@app.route('/whatsapp')
 def get_all_posts():
     user = request.args.get("user")
+    userId = request.args.get("userId")
     logged_in = client.get_score().split(':')
+    last_card = request.args.get("card")
+    game_action = request.args.get("action")
     result = []
+    sent_message = request.args.get("sent_message")
+
+    received_messagez = client.get_messages().split(":")
 
     i = 0
     while i < len(logged_in) - 1:
-        tuplezinha  = f"{logged_in[i]}. {logged_in[i + 1]}"
+        tuplezinha  = (logged_in[i], logged_in[i + 1])
         result.append(tuplezinha)
         i += 3
 
-    return render_template("index.html", logged_users=result, message_user=user)
+    result.append((0, "Todos"))
+
+    received_message = (getName( result,received_messagez[0]), received_messagez[1])
+
+    players = client.get_players().split(":")
+
+    players_ret = players_front(players, result)
+
+    
+
+    card = render_car(game_action, last_card)
+
+    return render_template("index.html", logged_users=result, message_user=user, message_userId=userId, sent_message=sent_message, received_message=received_message, players=players_ret, card=card)
+
+
+@app.route('/whatsapp')
+def sendMessage():
+    user = request.args.get("user")
+    userId = request.args.get("userId")
+    game_action = request.args.get("action")
+    last_card = request.args.get("card")
+    logged_in = client.get_score().split(':')
+    result = []
+    sent_message = request.args.get("sent_message")
+
+    if sent_message is not None and sent_message != "":
+        client.send_message(userId, sent_message)
+
+    received_messagez = client.get_messages().split(":")
+    received_message = (getName(received_messagez[0]), received_messagez[1])
+
+    i = 0
+    while i < len(logged_in) - 1:
+        tuplezinha  = (logged_in[i], logged_in[i + 1])
+        result.append(tuplezinha)
+        i += 3
+
+    result.append((0, "Todos"))
+
+    players = client.get_players().split(":")
+
+    players_ret = players_front(players, result)
+
+    card = render_car(game_action, last_card)
+
+    return render_template("index.html", logged_users=result, message_user=user, message_userId=userId, sent_message=sent_message, received_message=received_message, players=players_ret, card=card)
         
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
